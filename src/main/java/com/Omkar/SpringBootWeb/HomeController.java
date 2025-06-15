3bc478cae6610e969e9ced16c8fb5f1baae6f0c6
Omkar Kusekar <omkarkusekar12@gmail.com>
2025-06-15 12:22:28 +0530

    insted of Session obj used Model obj which is a interface

--- src/main/java/com/Omkar/SpringBootWeb/HomeController.java
+++ src/main/java/com/Omkar/SpringBootWeb/HomeController.java
@@ -3,7 +3,9 @@ package com.Omkar.SpringBootWeb;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
@@ -13,13 +15,14 @@ public class HomeController {
         return "index.jsp";
     }
     @RequestMapping("add")
-    public String add(int num1,int num2, HttpSession session){
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
 
 
-        int result=num1+num2+12;
+        int result=num1+num2;
         System.out.println(result);
 
-        session.setAttribute("result",result);
+
+        model.addAttribute("result",result);
         return "result.jsp";
     }
 }
